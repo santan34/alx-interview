707b1e9e4bf1f2ce1e864bafaa9383084925847a
St <aaronndas@gmail.com>
2024-08-09 01:48:07 +0200

    trail

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""minimum operations"""
+
+
+def minOperations(n):
+    """in operations"""
+    if n <= 1:
+        return 0
+
+    operations = 0
+    factor = 2
+
+    while n > 1:
+        while n % factor == 0:
+            operations += factor
+            n //= factor
+        factor += 1
+
+    return operations
