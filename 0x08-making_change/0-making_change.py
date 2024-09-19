53a2432b8b850a537a17010794073b1128c53dee
St <aaronndas@gmail.com>
2024-09-19 23:55:41 +0200

    done

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -3,26 +3,26 @@
 coin change
 """
 
+
 def makeChange(coins, total):
     """
     count the number of coins
     """
-    coins.sort()
-    coins = coins[::-1]
+    coins.sort(reverse=True)
     i = 0
     count = 0
     sum = 0
     init_tot = total
+    if total == 0:
+        return 0
     while i < len(coins):
-        if total-coins[i] >= 0:
-            count+=1
+        if total - coins[i] >= 0:
+            count += 1
             total -= coins[i]
-            sum +=  coins[i]
+            sum += coins[i]
         else:
-            i+=1
+            i += 1
 
     if sum != init_tot:
-        return -1 
+        return -1
     return count
-
-
