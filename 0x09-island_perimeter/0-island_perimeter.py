c84868769d9feee27c10f2068a1f46930f7545b3
St <aaronndas@gmail.com>
2024-09-27 01:29:05 +0200

    commented

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+"""ISLAND PERIMETER QUESTION"""
 
 def island_perimeter(grid):
+    """THE MAIN FUNCTION
+    """
+    
     total = 0
     for i in range(len(grid)):
         for j in range(len(grid[i])):
@@ -10,6 +14,7 @@ def island_perimeter(grid):
 
 
 def scanner(matrix, i, j):
+    """CHECKS NEIGBOURS"""
     sum = 0
     if (j - 1 >= 0 and matrix[i][j - 1] == 0):
         sum += 1
