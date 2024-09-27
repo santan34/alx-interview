8438a062f3313b291ca1dcad49c9524172b1a6f2
St <aaronndas@gmail.com>
2024-09-27 02:25:09 +0200

    autopeo

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -18,9 +18,9 @@ def island_perimeter(grid):
     for i in range(len(grid)):
         for j in range(len(grid[i])):
             if grid[i][j] == 1:
-                perimeter += 4  # Assume each land cell has a perimeter of 4
+                perimeter += 4
                 if i > 0 and grid[i - 1][j] == 1:
-                    perimeter -= 2  # Deduct 2 for shared sides with adjacent land
+                    perimeter -= 2
                 if j > 0 and grid[i][j - 1] == 1:
                     perimeter -= 2
     return perimeter
